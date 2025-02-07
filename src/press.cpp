4b3ae53194d8e7bd4a78be02b2acb45e51943814
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-02-07 17:01:23 +1300

    fix: faster press

--- src/press.cpp
+++ src/press.cpp
@@ -1,54 +1,37 @@
 #include <Rcpp.h>
+// [[Rcpp::plugins(openmp)]]
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 using namespace Rcpp;
 
+// Enable compiler optimizations
 // [[Rcpp::export]]
 SEXP press_cpp(NumericVector truth, NumericVector estimate, bool na_rm = true) {
-  
-  // Removing NA values if na_rm is TRUE
-  if (na_rm) {
-    
-    std::vector<int> naIndices;
-    
-    // Check for NaN in 'truth'
-    for (size_t i = 0; i < static_cast<size_t>(truth.size()); ++i) {
-      if (std::isnan(truth[i])) {
-        naIndices.push_back(i);
-      }
-    }
-    
-    // Check for NaN in 'estimate'
-    for (size_t i = 0; i < static_cast<size_t>(estimate.size()); ++i) {
-      if (std::isnan(estimate[i])) {
-        naIndices.push_back(i);
-      }
-    }
-    
-    // Sort the indices
-    std::sort(naIndices.begin(), naIndices.end());
-    
-    // Remove duplicates
-    auto last = std::unique(naIndices.begin(), naIndices.end());
-    naIndices.erase(last, naIndices.end());
-    
-    // Create new vectors without NA elements
-    NumericVector cleaned_truth(truth.size() - naIndices.size());
-    NumericVector cleaned_estimate(estimate.size() - naIndices.size());
-    
-    int index = 0;
-    for (int i = 0; i < truth.size(); ++i) {
-      if (!std::binary_search(naIndices.begin(), naIndices.end(), i)) {
-        cleaned_truth[index] = truth[i];
-        cleaned_estimate[index] = estimate[i];
-        index++;
-      }
-    }
-    
-    truth = cleaned_truth;
-    estimate = cleaned_estimate;
+  if (truth.size() != estimate.size()) {
+    stop("'truth' and 'estimate' must have the same length");
   }
   
-  double metric = sum(pow(truth - estimate, 2));
+  const int n = truth.length();
+  const double* t = REAL(truth);
+  const double* e = REAL(estimate);
+  double sum = 0.0;
   
-  return wrap(metric);
+  if (na_rm) {
+    #pragma omp parallel for reduction(+:sum) schedule(static) if(n > 1000)
+    for (int i = 0; i < n; i++) {
+      if (!ISNAN(t[i]) && !ISNAN(e[i])) {
+        const double diff = t[i] - e[i];
+        sum += diff * diff;
+      }
+    }
+  } else {
+    #pragma omp parallel for simd reduction(+:sum) schedule(static) if(n > 1000)
+    for (int i = 0; i < n; i++) {
+      const double diff = t[i] - e[i];
+      sum += diff * diff;
+    }
+  }
   
+  return wrap(sum);
 }
